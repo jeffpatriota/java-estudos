19f3aa1edd126fa2cb0f27008414942dac5a1c7a
Jefferson Patriota <77636115+jeffpatriota@users.noreply.github.com>
2021-07-23 20:24:11 -0300

    Add files via upload
    DesafioLogico

---
+++ DesafioLogicos.java
@@ -0,0 +1,21 @@
+package Fundamentos.operadores;
+
+public class DesafioLogicos {
+	public static void main(String[] args) {
+
+		boolean trabalho1 = true;
+		boolean trabalho2 = false;
+		
+		boolean comprouTV50 = trabalho1 && trabalho2;
+		boolean comprouTV32 = trabalho1 ^ trabalho2;
+		boolean comprouSorvete = trabalho1 || trabalho2;
+		boolean maisSaudavel = !comprouSorvete;
+		
+		System.out.println("Comprou TV 50\"?" + comprouTV50);
+		System.out.println("Comprou TV 32\"?" + comprouTV32);
+		System.out.println("Toumou Sorvete?" + comprouTV50);
+		System.out.println("Mais Saudável? " + maisSaudavel);
+
+	}
+
+}
