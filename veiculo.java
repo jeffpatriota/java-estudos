c66748ed9cb6df2f16dca743239b28b7dd1234b6
Jefferson Patriota <77636115+jeffpatriota@users.noreply.github.com>
2021-09-03 14:33:00 -0300

    Add files via upload
    POO

---
+++ veiculo.java
@@ -0,0 +1,32 @@
+package pessoas;
+
+public class veiculo {
+	private long id;
+	private String descricao;
+	private double valor;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+}
